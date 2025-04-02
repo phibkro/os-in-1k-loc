1e30c3bed23c3fe8fe6bb26dec107e8610483cb7
phibkro <71797726+phibkro@users.noreply.github.com>
2025-04-02 03:47:46 +0200

    feat: :sparkles: add trap handler

--- src/kernel.zig
+++ src/kernel.zig
@@ -35,12 +35,148 @@ export fn kernel_main() noreturn {
     const bss_len = @intFromPtr(bss) - @intFromPtr(bss_end);
     @memset(bss[0..bss_len], 0);
 
+    write_csr("stvec", @intFromPtr(&kernel_entry));
+    // asm volatile ("unimp");
+
     const hello = "\n\nhello kernel!\n";
 
     console.print(hello, .{}) catch {};
 
     // Finally, the function enters an infinite loop and the kernel terminates.
     while (true) asm volatile ("wfi");
+}
+
+export fn kernel_entry() align(4) callconv(.Naked) void {
+    asm volatile (
+        \\csrw sscratch, sp
+        \\addi sp, sp, -4 * 31
+        \\sw ra,  4 * 0(sp)
+        \\sw gp,  4 * 1(sp)
+        \\sw tp,  4 * 2(sp)
+        \\sw t0,  4 * 3(sp)
+        \\sw t1,  4 * 4(sp)
+        \\sw t2,  4 * 5(sp)
+        \\sw t3,  4 * 6(sp)
+        \\sw t4,  4 * 7(sp)
+        \\sw t5,  4 * 8(sp)
+        \\sw t6,  4 * 9(sp)
+        \\sw a0,  4 * 10(sp)
+        \\sw a1,  4 * 11(sp)
+        \\sw a2,  4 * 12(sp)
+        \\sw a3,  4 * 13(sp)
+        \\sw a4,  4 * 14(sp)
+        \\sw a5,  4 * 15(sp)
+        \\sw a6,  4 * 16(sp)
+        \\sw a7,  4 * 17(sp)
+        \\sw s0,  4 * 18(sp)
+        \\sw s1,  4 * 19(sp)
+        \\sw s2,  4 * 20(sp)
+        \\sw s3,  4 * 21(sp)
+        \\sw s4,  4 * 22(sp)
+        \\sw s5,  4 * 23(sp)
+        \\sw s6,  4 * 24(sp)
+        \\sw s7,  4 * 25(sp)
+        \\sw s8,  4 * 26(sp)
+        \\sw s9,  4 * 27(sp)
+        \\sw s10, 4 * 28(sp)
+        \\sw s11, 4 * 29(sp)
+        \\
+        \\csrr a0, sscratch
+        \\sw a0, 4 * 30(sp)
+        \\
+        \\mv a0, sp
+        \\call handle_trap
+        \\
+        \\lw ra,  4 * 0(sp)
+        \\lw gp,  4 * 1(sp)
+        \\lw tp,  4 * 2(sp)
+        \\lw t0,  4 * 3(sp)
+        \\lw t1,  4 * 4(sp)
+        \\lw t2,  4 * 5(sp)
+        \\lw t3,  4 * 6(sp)
+        \\lw t4,  4 * 7(sp)
+        \\lw t5,  4 * 8(sp)
+        \\lw t6,  4 * 9(sp)
+        \\lw a0,  4 * 10(sp)
+        \\lw a1,  4 * 11(sp)
+        \\lw a2,  4 * 12(sp)
+        \\lw a3,  4 * 13(sp)
+        \\lw a4,  4 * 14(sp)
+        \\lw a5,  4 * 15(sp)
+        \\lw a6,  4 * 16(sp)
+        \\lw a7,  4 * 17(sp)
+        \\lw s0,  4 * 18(sp)
+        \\lw s1,  4 * 19(sp)
+        \\lw s2,  4 * 20(sp)
+        \\lw s3,  4 * 21(sp)
+        \\lw s4,  4 * 22(sp)
+        \\lw s5,  4 * 23(sp)
+        \\lw s6,  4 * 24(sp)
+        \\lw s7,  4 * 25(sp)
+        \\lw s8,  4 * 26(sp)
+        \\lw s9,  4 * 27(sp)
+        \\lw s10, 4 * 28(sp)
+        \\lw s11, 4 * 29(sp)
+        \\lw sp,  4 * 30(sp)
+        \\sret
+    );
+}
+
+const TrapFrame = extern struct {
+    ra: usize,
+    gp: usize,
+    tp: usize,
+    t0: usize,
+    t1: usize,
+    t2: usize,
+    t3: usize,
+    t4: usize,
+    t5: usize,
+    t6: usize,
+    a0: usize,
+    a1: usize,
+    a2: usize,
+    a3: usize,
+    a4: usize,
+    a5: usize,
+    a6: usize,
+    a7: usize,
+    s0: usize,
+    s1: usize,
+    s2: usize,
+    s3: usize,
+    s4: usize,
+    s5: usize,
+    s6: usize,
+    s7: usize,
+    s8: usize,
+    s9: usize,
+    s10: usize,
+    s11: usize,
+    sp: usize,
+};
+
+export fn handle_trap(trap_frame: *const TrapFrame) void {
+    _ = trap_frame;
+
+    const scause = read_csr("scause"); // mcause
+    const stval = read_csr("stval"); // mepc
+    const user_pc = read_csr("sepc"); // utvec
+
+    std.debug.panic("Unexpected trap scause={x}, stval={x}, sepc={x}\n", .{ scause, stval, user_pc });
+}
+
+fn read_csr(comptime reg: []const u8) usize {
+    return asm ("csrr %[ret], " ++ reg
+        : [ret] "=r" (-> usize),
+    );
+}
+
+fn write_csr(comptime reg: []const u8, val: usize) void {
+    asm volatile ("csrw " ++ reg ++ ", %[val]"
+        :
+        : [val] "r" (val),
+    );
 }
 
 const console: std.io.AnyWriter = .{
